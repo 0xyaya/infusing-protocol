3902704a4dbc63884bcefab1648a63c9422f79b7
yann <yann.thibault1@gmail.com>
2023-10-26 19:15:57 +0200

    refactor controller

--- programs/infusing-protocol/src/instructions/infuse.rs
+++ programs/infusing-protocol/src/instructions/infuse.rs
@@ -20,7 +20,6 @@ pub struct Infuse<'info> {
     pub nft_mint: UncheckedAccount<'info>,
     #[account(init_if_needed, seeds = [ b"infused-account", nft_mint.key().as_ref()], payer = signer, space = 8 + InfusedAccount::SIZE, bump)]
     pub infused_account: Account<'info, InfusedAccount>,
-
     /// CHECK: This account is not read or written
     #[account(mut)]
     pub fees_account: AccountInfo<'info>,
@@ -29,85 +28,102 @@ pub struct Infuse<'info> {
     pub system_program: Program<'info, System>,
 }
 
+impl<'info> Infuse<'info> {
+    pub fn transfer_fees(&self, fees: f64) -> Result<()> {
+        let fees_transfer_instruction =
+            system_instruction::transfer(self.signer.key, self.fees_account.key, fees as u64);
+
+        invoke_signed(
+            &fees_transfer_instruction,
+            &[
+                self.signer.to_account_info(),
+                self.fees_account.to_account_info(),
+                self.system_program.to_account_info(),
+            ],
+            &[],
+        )?;
+        Ok(())
+    }
+
+    pub fn transfer_strategies(
+        &self,
+        remaining_accounts: &[AccountInfo<'info>],
+        amount: u64,
+    ) -> Result<()> {
+        if remaining_accounts.len() != self.controller_details.strategies.len() {
+            return Err(ErrorCode::InvalidRemainingAccountsLength.into());
+        }
+
+        for i in 0..remaining_accounts.len() {
+            let holding_account = &remaining_accounts[i];
+            let strategy = self
+                .controller_details
+                .strategies
+                .iter()
+                .find(|s| s.holding_account == *holding_account.key)
+                .unwrap();
+
+            if !strategy.active {
+                continue;
+            }
+
+            let share_amount = amount as f64 * (strategy.weight as f64 / 100.00);
+            let holding_transfer_instruction = system_instruction::transfer(
+                self.signer.key,
+                holding_account.key,
+                share_amount as u64,
+            );
+
+            invoke_signed(
+                &holding_transfer_instruction,
+                &[
+                    self.signer.to_account_info(),
+                    holding_account.to_account_info(),
+                    self.system_program.to_account_info(),
+                ],
+                &[],
+            )?;
+        }
+
+        Ok(())
+    }
+
+    pub fn update_account(&mut self, amount: u64) -> Result<AccountInfused> {
+        let time = Clock::get().unwrap().unix_timestamp as u64;
+        let price = 1.40 as f64 / 29 as f64;
+        let carbon_tons = (amount as f64 / (LAMPORTS_PER_SOL as f64 * price)) as u64;
+
+        self.infused_account.carbon_score = self
+            .infused_account
+            .carbon_score
+            .checked_add(carbon_tons)
+            .unwrap();
+        self.infused_account.nft_mint = self.nft_mint.key();
+        self.infused_account.last_infused_time = time;
+
+        Ok(AccountInfused {
+            amount: carbon_tons,
+            nft_mint: *self.nft_mint.key,
+            time: time,
+        })
+    }
+}
+
 pub fn infuse_handler<'info>(
     ctx: Context<'_, '_, '_, 'info, Infuse<'info>>,
     amount: u64,
 ) -> Result<()> {
-    let controller_details = &mut ctx.accounts.controller_details;
-    let infused_account = &mut ctx.accounts.infused_account;
-    let fees_account = &mut ctx.accounts.fees_account;
-    let signer = &mut ctx.accounts.signer;
     let lamports = amount.checked_mul(LAMPORTS_PER_SOL).unwrap();
     let fees = lamports as f64 * 0.04;
     let amount_to_burn = lamports.checked_sub(fees as u64).unwrap();
-    let fees_transfer_instruction =
-        system_instruction::transfer(signer.key, fees_account.key, fees as u64);
-    // filter controller strategies to only active ones
-    // let strategies = controller
-    //     .strategies
-    //     .iter()
-    //     .filter(|s| s.active)
-    //     .collect::<Vec<_>>();
 
-    if ctx.remaining_accounts.len() != controller_details.strategies.len() {
-        return Err(ErrorCode::InvalidRemainingAccountsLength.into());
-    }
+    ctx.accounts.transfer_fees(fees)?;
 
-    for i in 0..ctx.remaining_accounts.len() {
-        let holding_account = &ctx.remaining_accounts[i];
-        let strategy = controller_details
-            .strategies
-            .iter()
-            .find(|s| s.holding_account == *holding_account.key)
-            .unwrap();
+    ctx.accounts
+        .transfer_strategies(ctx.remaining_accounts, amount)?;
 
-        if !strategy.active {
-            continue;
-        }
+    let event = ctx.accounts.update_account(amount_to_burn)?;
+    emit!(event);
 
-        let amount = amount_to_burn as f64 * (strategy.weight as f64 / 100.00);
-        msg!("holding_account: {}", holding_account.key);
-        msg!("amount: {}", amount);
-        let holding_transfer_instruction =
-            system_instruction::transfer(signer.key, holding_account.key, amount as u64);
-
-        invoke_signed(
-            &holding_transfer_instruction,
-            &[
-                signer.to_account_info(),
-                holding_account.to_account_info(),
-                ctx.accounts.system_program.to_account_info(),
-            ],
-            &[],
-        )?;
-    }
-
-    invoke_signed(
-        &fees_transfer_instruction,
-        &[
-            signer.to_account_info(),
-            ctx.accounts.fees_account.to_account_info(),
-            ctx.accounts.system_program.to_account_info(),
-        ],
-        &[],
-    )?;
-
-    // TODO: use switchboard price feed
-    let price = 1.40 as f64 / 29 as f64;
-
-    let carbon_tons = (amount_to_burn as f64 / (LAMPORTS_PER_SOL as f64 * price)) as u64;
-    let time = Clock::get().unwrap().unix_timestamp as u64;
-    infused_account.carbon_score = infused_account
-        .carbon_score
-        .checked_add(carbon_tons)
-        .unwrap();
-    infused_account.nft_mint = ctx.accounts.nft_mint.key();
-    infused_account.last_infused_time = time;
-
-    emit!(AccountInfused {
-        amount: carbon_tons,
-        nft_mint: *ctx.accounts.nft_mint.key,
-        time: time,
-    });
     Ok(())
 }
